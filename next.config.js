f7d7c6ce33560733b8ea512814afbf0c40e45fd3
phiresky <phireskyde+git@gmail.com>
2023-12-11 17:48:40 +0100

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/lemmy-federation-state",
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
